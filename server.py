fdda5de014d2fec9b3fec4b1572c6a7f894b0d35
zarix908 <daniyarrus@gmail.com>
2020-04-15 14:16:40 +0500

    some fixes

--- server.py
+++ server.py
@@ -1,26 +1,22 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder='.')
 CORS(app)
 
-icons = {"icons": [False, False]}
+icons = [False, False]
 
 @app.route('/')
 def index():
-	return render_template('index.html')
-
-@app.route('/icons')
-def get_icons():
-	return json.dumps(icons)
+	return render_template('index.html', icons=icons)
 
 @app.route('/heart/enable')
 def enable_heart():
-	icons["icons"][0] = True
-	return json.dumps(icons) 
+	icons[0] = True
+	return redirect(url_for('index')) 
 
 @app.route('/facebook/enable')
 def enable_facebook():
-	icons["icons"][1] = True
-	return json.dumps(icons) 
+	icons[1] = True
+	return redirect(url_for('index')) 
